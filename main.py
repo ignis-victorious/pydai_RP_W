810c541d2d401ac04b55994cec8a200eb0177d6f
Emagnu <ignisvictorious@gmail.com>
2026-04-02 15:07:39 +0100

    Second commit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #
 #  Import LIBRARIES
-from pydantic_ai import Agent
+from pydantic_ai import Agent, AgentRunResult
 
 #  Import FILES
 #  ______________________
 #
 
 
-agent = Agent(
-    "google-gla:gemini-2.5-flash",
+agent: Agent = Agent(
+    model="google-gla:gemini-2.5-flash",
     instructions="You're a Python Expert. Reply in one sentence.",
 )
 
-result = agent.run_sync("What is Pydantic AI?")
+result: AgentRunResult[str] = agent.run_sync(user_prompt="What is Pydantic AI?")
 print(result.output)
 
 
